2ada544255755f02002e35df7608381a94bc4b00
JiangChao <explosionZing@126.com>
2020-09-10 20:14:20 +0800

    first commit

---
+++ pages/cart/index.vue
@@ -0,0 +1,24 @@
+<template>
+	<view>
+		购物车
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	
+</style>
